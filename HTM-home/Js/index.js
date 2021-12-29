46311e11c6e88cf49a48ef978c49a5d6e09b2662
Vishnu S <68739148+S0Vishnu@users.noreply.github.com>
2021-12-29 16:49:16 +0530

    Update index.js

--- HTM-home/Js/index.js
+++ HTM-home/Js/index.js
@@ -1,1 +1,20 @@
-c
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelector('.nav-links li');
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+});
+
+var lastScrollTop = 0
+navbar = document.getElementById("navbar");
+window.addEventListener("scroll", function() {
+    var scrollTop = window.pageYOffset || this.document
+        .documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+        navbar.style.top = "-80px"
+    } else {
+        navbar.style.top = "0"
+    }
+    lastScrollTop = scrollTop;
+})
