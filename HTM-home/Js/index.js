dd23a8c7dc0250c656f64ecef0ccd1f8147aa93a
Vishnu S <68739148+S0Vishnu@users.noreply.github.com>
2021-12-30 20:17:09 +0530

    Add files via upload

--- HTM-home/Js/index.js
+++ HTM-home/Js/index.js
@@ -2,19 +2,21 @@ const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelector('.nav-links li');
 
-hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("open");
-});
 
 var lastScrollTop = 0
 navbar = document.getElementById("navbar");
 window.addEventListener("scroll", function() {
     var scrollTop = window.pageYOffset || this.document
         .documentElement.scrollTop;
-    if (scrollTop > lastScrollTop) {
-        navbar.style.top = "-80px"
-    } else {
-        navbar.style.top = "0"
+    if (!navLinks.classList.contains("open")) {
+        if (scrollTop > lastScrollTop) {
+            navbar.style.top = "-80px"
+        } else {
+            navbar.style.top = "0"
+        }
+        lastScrollTop = scrollTop;
     }
-    lastScrollTop = scrollTop;
-})
+});
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+});
